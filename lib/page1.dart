9eb56631d3682b8b869e1eb07d5359d52eac8586
harry asher <harryasher11@gmail.com>
2023-10-22 18:43:48 +0530

    first commit

---
+++ lib/page1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'listofsub.dart';
+
+class Custompage extends StatelessWidget {
+  const Custompage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        toolbarHeight: 80.0,
+        title: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Class 12',
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Books',
+                style: TextStyle(fontSize: 16),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: const Listpage(),
+    );
+  }
+}
---
+++ lib/page2.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'downloadpage.dart';
+
+class Classroom extends StatefulWidget {
+  final String text;
+  final String path;
+  const Classroom({super.key, required this.text, required this.path});
+
+  @override
+  State<Classroom> createState() => ClassroomState();
+}
+
+class ClassroomState extends State<Classroom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            )),
+        backgroundColor: Colors.white,
+        title: Text(
+          widget.text,
+          style: const TextStyle(fontSize: 16, color: Colors.black),
+        ),
+      ),
+      body: Subjectpage(path: widget.text),
+    );
+  }
+}
